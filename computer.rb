53ce1ce6f22f41f4c4bfaf5aa5147bc674137f9b
William Sosropartono <wcsosropartono@gmail.com>
2022-06-27 18:10:05 -0700

    Completed Player Side implemenation

--- computer.rb
+++ computer.rb
@@ -1,12 +1,14 @@
 require 'set'
 
 class Computer
-  attr_reader :numbers, :secret_code, :guess
+  attr_reader :numbers, :secret_code, :guess, :set, :tries
 
   def initialize
     @secret_code = []
     @guess = []
     @response = []
+    @set = Set.new(1111..6666)
+    @tries = 0
     @numbers = %w[1 2 3 4 5 6]
   end
 
@@ -17,11 +19,21 @@ class Computer
     end
   end
 
-  def make_move
-    @computer_guess = [spot1, spot2, spot3, spot4]
-    set = Set.new(1111..6666)
-
+  def first_move
+    @secret_code = [1, 1, 2, 2]
   end
 
-  def mini_max end
+  def calculate_score(black, white, none)
+
+  def min_max(black, white, none)
+    if 
+    if none
+    score
+  end
+
+  def make_move
+    @secret_code = [1, 1, 2, 2] if @tries == 0
+
+    # @secret_code = 
+  end
 end
